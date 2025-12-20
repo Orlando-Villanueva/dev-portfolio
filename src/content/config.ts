1f7cc2408a0dc81bd146a3e86aac1f213728f292
Orlando Villanueva <orlandovillanueva11@gmail.com>
2025-12-19 20:22:11 -0500

    feat: migrate images to Astro assets for optimized loading and use Image component

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,13 +2,13 @@ import { defineCollection, z } from 'astro:content';
 
 const projects = defineCollection({
     type: 'data',
-    schema: z.object({
+    schema: ({ image }) => z.object({
         title: z.string(),
         tagline: z.string(),
         problem: z.string(),
         solution: z.string(),
         result: z.string(),
-        image: z.string(),
+        image: image(),
         tags: z.array(z.string()),
         link: z.string().optional(),
     }),
